48041043c906ecee2ffbecc80120c40450b39d54
Federico Rossi <federicomrossi@gmail.com>
2014-09-27 15:41:51 -0300

    Mejorado el panel derecho de información y pseudocodigo. Agregado dinámico de contenido en las pestañas.

--- Codigo/papers/src/main/java/ar/com/taller2/papers/AprendiendoGrafos2.java
+++ Codigo/papers/src/main/java/ar/com/taller2/papers/AprendiendoGrafos2.java
@@ -46,6 +46,10 @@ import javax.swing.JSpinner;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.Component;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class AprendiendoGrafos2 extends JApplet {
 
@@ -53,7 +57,7 @@ public class AprendiendoGrafos2 extends JApplet {
 	private static final Logger logger = LogManager.getLogger();
 	
 	private static final Color DEFAULT_BG_COLOR = Color.decode("#FAFBFF");
-    private static final Dimension DEFAULT_SIZE = new Dimension(530, 320);
+    private static final Dimension DEFAULT_SIZE = new Dimension(800, 320);
     
     
     private JGraphModelAdapter m_jgAdapter;
@@ -65,8 +69,9 @@ public class AprendiendoGrafos2 extends JApplet {
 	
 	/**
 	 * Create the applet.
+	 * @throws IOException 
 	 */
-	public AprendiendoGrafos2() {
+	public AprendiendoGrafos2() throws IOException {
 		
 		try {
             // Set System L&F
@@ -295,28 +300,55 @@ public class AprendiendoGrafos2 extends JApplet {
 		JTabbedPane tbdPaneDerecha = new JTabbedPane(JTabbedPane.TOP);
 		splitPane_2.setRightComponent(tbdPaneDerecha);
 		
-		JPanel panel = new JPanel();
-		tbdPaneDerecha.addTab("Información", null, panel, null);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		JPanel panelInformacion = new JPanel();
+		tbdPaneDerecha.addTab("Información", null, panelInformacion, null);
+		panelInformacion.setLayout(new BoxLayout(panelInformacion, BoxLayout.Y_AXIS));
 		
-		JLabel label_1 = new JLabel("Título");
-		panel.add(label_1);
+		JLabel lblTituloInformacion = new JLabel("Algoritmo de Dijkstra");
+		lblTituloInformacion.setBorder(new EmptyBorder(10, 10, 10, 10));
+		lblTituloInformacion.setFont(new Font("Tahoma", Font.BOLD, 14));
+		panelInformacion.add(lblTituloInformacion);
 		
-		JTextPane textPane = new JTextPane();
-		panel.add(textPane);
+		JTextPane textPaneContenidoInformacion = new JTextPane();
+		textPaneContenidoInformacion.setContentType("text/html");
+		textPaneContenidoInformacion.setAlignmentX(Component.LEFT_ALIGNMENT);
+		textPaneContenidoInformacion.setFont(new Font("Tahoma", Font.PLAIN, 11));
+//		textPaneContenidoInformacion.setText("<html>\r\n<body>\r\n\t<div id=\"mw-content-text\" lang=\"es\" dir=\"ltr\" class=\"mw-content-ltr\">\r\n\t<p>El <b>algoritmo de Dijkstra</b>, también llamado <b>algoritmo de caminos mínimos</b>, es un <a href=\"http://es.wikipedia.org/wiki/Algoritmo\" title=\"Algoritmo\">algoritmo</a> para la determinación del <a href=\"http://es.wikipedia.org/wiki/Problema_de_los_caminos_m%C3%A1s_cortos\" title=\"Problema de los caminos más cortos\" class=\"mw-redirect\">camino más corto</a> dado un <a href=\"http://es.wikipedia.org/wiki/V%C3%A9rtice_(Teor%C3%ADa_de_grafos)\" title=\"Vértice (Teoría de grafos)\" class=\"mw-redirect\">vértice</a> origen al resto de vértices en un <a href=\"http://es.wikipedia.org/wiki/Grafo\" title=\"Grafo\">grafo</a> con pesos en cada <a href=\"http://es.wikipedia.org/wiki/Arista_(Teor%C3%ADa_de_grafos)\" title=\"Arista (Teoría de grafos)\" class=\"mw-redirect\">arista</a>. Su nombre se refiere a <a href=\"http://es.wikipedia.org/wiki/Edsger_Dijkstra\" title=\"Edsger Dijkstra\">Edsger Dijkstra</a>, quien lo describió por primera vez en 1959.</p>\r\n\t<p>La idea subyacente en este algoritmo consiste en ir explorando todos los caminos más cortos que parten del vértice origen y que llevan a todos los demás vértices; cuando se obtiene el camino más corto desde el vértice origen, al resto de vértices que componen el grafo, el algoritmo se detiene. El algoritmo es una especialización de la búsqueda de costo uniforme, y como tal, no funciona en grafos con aristas de coste negativo (al elegir siempre el nodo con distancia menor, pueden quedar excluidos de la búsqueda nodos que en próximas iteraciones bajarían el costo general del camino al pasar por una arista con costo negativo).</p>\r\n\t<p></p>\r\n\t<div id=\"toc\" class=\"toc\">\r\n\t<div id=\"toctitle\">\r\n\t\r\n\t<h2><span class=\"mw-headline\" id=\"Algoritmo\">Algoritmo</span></h2>\r\n\t<p>Teniendo un grafo dirigido ponderado de N nodos no aislados, sea x el nodo inicial, un vector D de tamaño N guardará al final del algoritmo las distancias desde x al resto de los nodos.</p>\r\n\t<ol>\r\n\t<li>Inicializar todas las distancias en D con un valor infinito relativo ya que son desconocidas al principio, exceptuando la de x que se debe colocar en 0 debido a que la distancia de x a x sería 0.</li>\r\n\t<li>Sea a = x (tomamos a como nodo actual).</li>\r\n\t<li>Recorremos todos los nodos adyacentes de <i>a</i>, excepto los nodos marcados, llamaremos a estos nodos no marcados v<sub>i</sub>.</li>\r\n\t<li>Para el nodo actual, calculamos la distancia tentativa desde dicho nodo a sus vecinos con la siguiente fórmula: dt(v<sub>i</sub>) = D<sub>a</sub> + d(a,v<sub>i</sub>). Es decir, la distancia tentativa del nodo ‘v<sub>i</sub>’ es la distancia que actualmente tiene el nodo en el vector D más la distancia desde dicho el nodo ‘a’ (el actual) al nodo v<sub>i</sub>. Si la distancia tentativa es menor que la distancia almacenada en el vector, actualizamos el vector con esta distancia tentativa. Es decir: Si dt(v<sub>i</sub>) &lt; D<sub>vi</sub> → D<sub>vi</sub> = dt(v<sub>i</sub>)</li>\r\n\t<li>Marcamos como completo el nodo a.</li>\r\n\t<li>Tomamos como próximo nodo actual el de menor valor en D (puede hacerse almacenando los valores en una cola de prioridad) y volvemos al paso 3 mientras existan nodos no marcados.</li>\r\n\t</ol>\r\n\t<p>Una vez terminado al algoritmo, D estará completamente lleno.</p>\r\n\r\n\t<h2><span class=\"mw-headline\" id=\"Complejidad\">Complejidad</span></h2>\r\n\t<p>Orden de complejidad del algoritmo: <i>O</i>(|<i>V</i>|<sup>2</sup>+|E|) = <i>O</i>(|<i>V</i>|<sup>2</sup>) sin utilizar cola de prioridad, <i>O</i>((|<i>E</i>|+|<i>V</i>|) log |<i>V</i>|) utilizando cola de prioridad (por ejemplo un montículo).</p>\r\n\t<p>Podemos estimar la complejidad computacional del algoritmo de Dijkstra (en términos de sumas y comparaciones). El <a href=\"http://es.wikipedia.org/wiki/Algoritmo\" title=\"Algoritmo\">algoritmo</a> realiza a lo más n-1 iteraciones, ya que en cada iteración se añade un vértice al conjunto distinguido. Para estimar el número total de operaciones basta estimar el número de operaciones que se llevan a cabo en cada iteración. Podemos identificar el vértice con la menor etiqueta entre los que no están en S<sub>k</sub> realizando n-1 comparaciones o menos. Después hacemos una suma y una comparación para actualizar la etiqueta de cada uno de los vértices que no están en S<sub>k</sub>. Por tanto, en cada iteración se realizan a lo sumo 2(n-1) operaciones, ya que no puede haber más de n-1 etiquetas por actualizar en cada iteración. Como no se realizan más de n-1 iteraciones, cada una de las cuales supone a lo más 2(n-1) operaciones, llegamos al siguiente teorema.</p>\r\n\t<p><i><b>TEOREMA:</b></i> El Algoritmo de Dijkstra realiza O(n<sup>2</sup>) operaciones (sumas y comparaciones) para determinar la longitud del camino más corto entre dos vértices de un grafo ponderado simple, conexo y no dirigido con n vértices.</p>\r\n\r\n</body>\r\n</html>");
+		textPaneContenidoInformacion.setEditable(false);
+		textPaneContenidoInformacion.setBorder(new EmptyBorder(10, 10, 10, 10));
+		panelInformacion.add(textPaneContenidoInformacion);
 		
-		JPanel panel_1 = new JPanel();
-		tbdPaneDerecha.addTab("Algoritmo", null, panel_1, null);
-		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
+		JScrollPane scrollPaneInformacion = new JScrollPane(textPaneContenidoInformacion);
+		scrollPaneInformacion.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panelInformacion.add(scrollPaneInformacion);
 		
-		JLabel label_2 = new JLabel("Pseudocódigo");
-		panel_1.add(label_2);
 		
-		JTextPane textPane_2 = new JTextPane();
-		textPane_2.setText("lala");
-		panel_1.add(textPane_2);
+		JPanel panelPseudocodigo = new JPanel();
+		tbdPaneDerecha.addTab("Algoritmo", null, panelPseudocodigo, null);
+		panelPseudocodigo.setLayout(new BoxLayout(panelPseudocodigo, BoxLayout.Y_AXIS));
 		
-		this.setSize(900, 900);
+		JLabel lblTituloAlgoritmo = new JLabel("Algoritmo de Dijkstra");
+		lblTituloAlgoritmo.setBorder(new EmptyBorder(10, 10, 10, 10));
+		lblTituloAlgoritmo.setFont(new Font("Tahoma", Font.BOLD, 14));
+		panelPseudocodigo.add(lblTituloAlgoritmo);
+		
+		JTextPane textPaneContenidoAlgoritmo = new JTextPane();
+		textPaneContenidoAlgoritmo.setContentType("text/html");
+//		textPaneContenidoAlgoritmo.setText("<html>\r\n<body>\r\n\r\n<p>Estructura de datos auxiliar: Q = Estructura de datos <a href=\"http://es.wikipedia.org/wiki/Cola_de_prioridad_(estructura_de_datos)\" title=\"Cola de prioridad (estructura de datos)\" class=\"mw-redirect\">Cola de prioridad</a> (se puede implementar con un <a href=\"http://es.wikipedia.org/wiki/Mont%C3%ADculo_(programaci%C3%B3n)\" title=\"Montículo (programación)\" class=\"mw-redirect\">montículo</a>)</p>\r\n<br/>\r\n<pre>\r\n<b>DIJKSTRA</b> (Grafo <i>G</i>, nodo_fuente <i>s</i>)\r\n  <b>para</b> <i>u</i> ∈ <i>V[G]</i> <b>hacer</b>\r\n    distancia[<i>u</i>] = INFINITO\r\n    padre[<i>u</i>] = NULL\r\n  distancia[<i>s</i>] = 0\r\n  adicionar (cola, (s, distancia[<i>s</i>]))\r\n  <b>mientras que</b> cola no es vacía <b>hacer</b>\r\n    <i>u</i> = extraer_mínimo(cola)\r\n    <b>para</b> todos <i>v</i> ∈ adyacencia[<i>u</i>] <b>hacer</b>\r\n      <b>si</b> distancia[<i>v</i>] &gt; distancia[<i>u</i>] + peso (u, v) <b>hacer</b>\r\n        distancia[<i>v</i>] = distancia[<i>u</i>] + peso (u, v)\r\n        padre[<i>v</i>] = <i>u</i>\r\n        adicionar(cola,(v, distancia[<i>v</i>]))\r\n</pre>\r\n\r\n</body>\r\n</html>");
+		textPaneContenidoAlgoritmo.setAlignmentX(Component.LEFT_ALIGNMENT);
+		textPaneContenidoAlgoritmo.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		textPaneContenidoAlgoritmo.setEditable(false);
+		textPaneContenidoAlgoritmo.setBorder(new EmptyBorder(10, 10, 10, 10));
+		panelPseudocodigo.add(textPaneContenidoAlgoritmo);
+		
+		JScrollPane scrollPaneAlgoritmo = new JScrollPane(textPaneContenidoAlgoritmo, 
+														  JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+														  JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPaneAlgoritmo.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panelPseudocodigo.add(scrollPaneAlgoritmo);
+		
+		
+		this.setSize(1100, 900);
 		splitPane_1.setResizeWeight(0.03d);
 		splitPane_2.setResizeWeight(0.8d);
 		splitPane_3.setResizeWeight(0.90d);
@@ -362,17 +394,14 @@ public class AprendiendoGrafos2 extends JApplet {
         positionVertexAt( "v4", 380, 70 );
 
         // that's all there is to it!...
+
         
-//        try {
-//            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-//                if ("Nimbus".equals(info.getName())) {
-//                    UIManager.setLookAndFeel(info.getClassName());
-//                    break;
-//                }
-//            }
-//        } catch (Exception e) {
-//            // If Nimbus is not available, you can set the GUI to another look and feel.
-//        }
+        //
+        // TEMP: Carga inicial de la info y el pseudocodigo. En el futuro, cada clase tendrá su resource almacenada y se le pedira el contenido
+        //
+        textPaneContenidoInformacion.setPage(AprendiendoGrafos2.class.getResource("/algorithms/dijkstra-info.html"));
+        textPaneContenidoAlgoritmo.setPage(AprendiendoGrafos2.class.getResource("/algorithms/dijkstra-pseudocode.html"));
+        // END TEMP
 	}
 
 	

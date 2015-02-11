ff8d3ca19315b626749861baeedd0a3766e03253
PabloR <rodriguez.pabloariel@gmail.com>
2015-02-11 10:38:25 -0300

    Dialogos para generar el grafo aleatorio

--- Codigo/papers/src/main/java/ar/com/taller2/papers/controller/MenuArchivoNuevoOrientadoAleatorioActionListener.java
+++ Codigo/papers/src/main/java/ar/com/taller2/papers/controller/MenuArchivoNuevoOrientadoAleatorioActionListener.java
@@ -1,8 +1,12 @@
 package ar.com.taller2.papers.controller;
 
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.SwingUtilities;
+
+import ar.com.taller2.papers.AleatoryDialog;
 import ar.com.taller2.papers.controller.jgraphx.ChangeWeightListener;
 import ar.com.taller2.papers.controller.jgraphx.MoveCellListener;
 import ar.com.taller2.papers.controller.jgraphx.NewEdgeListener;
@@ -17,8 +21,9 @@ AprendiendoGrafos app;
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		int vertices = 4;
-		int aristas = 2;
+		AleatoryDialog dialog = new AleatoryDialog((Frame)SwingUtilities.windowForComponent(app.getVista()));
+		int vertices = Integer.parseInt(dialog.getCantVertices());
+		int aristas = Integer.parseInt(dialog.getCantAristas());
 		this.app.getModelo().nuevoGrafoDirigidoAleatorio(vertices, aristas);
 		this.app.getVista().setGraph(app.getModelo().getGraph());
 		this.app.getVista().addNewEdgeListener(new NewEdgeListener(app));
